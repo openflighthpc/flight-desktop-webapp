b82836ccce0148332adf2e295d0c4bc938c5c8d5
Ben Armston <ben.armston@alces-software.com>
2020-04-01 16:46:00 +0100

    Obfuscate the shared session storage

--- src/shareSessionStorage.js
+++ src/shareSessionStorage.js
@@ -1,30 +1,36 @@
 // Share session storage between tabs.
 //
-// This function should be called when a new tab opens.  It "broadcasts" a
-// message to all other tabs sharing the same localStorage.  Other tabs
-// respond by "broadcasting" the session storage to all other tabs (including
-// the requesting tab) sharing the same localStorage.
+// This function should be called when a new tab opens.  It creates a
+// `BroadcastChannel` and requests the session data from any other tabs open
+// on this origin.  Other tabs respond by with the session data on the same
+// `BroadcastChannel`.
+//
+// The broadcast channel is shared across all tabs on the same origin.  We
+// obfuscate the channel name to make it harder for other apps on the same
+// origin to sniff the messages.
 //
 function shareSessionStorage() {
   if (typeof window === 'undefined') { return; }
+  if (typeof BroadcastChannel === 'undefined') { return; }
+
+  const channelName = process.env.REACT_APP_CHANNEL_NAME;
+  const bc = new BroadcastChannel(channelName);
 
   if (!sessionStorage.length) {
-    localStorage.setItem('requestSessionStorage', Date.now());
+    bc.postMessage({ key: 'requestSessionStorage' });
   };
 
-  window.addEventListener('storage', function(event) {
-    if (event.key === 'requestSessionStorage') {
-      localStorage.setItem('sendSessionStorage', JSON.stringify(sessionStorage));
-      localStorage.removeItem('sendSessionStorage');
-
-    } else if (event.key === 'sendSessionStorage' && !sessionStorage.length) {
-      const data = JSON.parse(event.newValue);
-
+  bc.onmessage = function(event) {
+    if (event.data.key === 'requestSessionStorage') {
+      bc.postMessage({ key: 'sendSessionStorage', value: JSON.stringify(sessionStorage) });
+    }
+    if (event.data.key === 'sendSessionStorage' && !sessionStorage.length) {
+      const data = JSON.parse(event.data.value);
       for (const key in data) {
         sessionStorage.setItem(key, data[key]);
       }
     }
-  });
+  }
 }
 
 export default shareSessionStorage;
