d49e194d9cd1361fb079e29293d170fa5d60d27d
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2023-12-07 14:40:31 +0000

    Update intro card to simplify and add new desktop button

--- src/SessionsPage.js
+++ src/SessionsPage.js
@@ -52,8 +52,10 @@ function NoSessionsFound() {
   return (
     <div>
       <p>
-        No sessions found.  You may want to <Link to="/sessions/new">start a
-          new session</Link>.
+        No sessions found.  You may want to
+        <Link to="/sessions/new">
+          start a new session
+        </Link>.
       </p>
     </div>
   );
@@ -92,23 +94,26 @@ function SessionsList({ reload, sessions }) {
 
   return (
     <React.Fragment>
-      <IntroCard sessions={sessions} />
+      <InfoRow sessions={sessions} />
       {decks}
     </React.Fragment>
   );
 }
 
-function IntroCard({ sessions }) {
+function InfoRow({ sessions }) {
   const sessionOrSessions = sessions.length === 1 ? 'session' : 'sessions';
 
   return (
-    <div className={`${styles.IntroCard} card card-body mb-2`}>
-      <p className={`${styles.IntroCardText} card-text`}>
-        You have {sessions.length} currently running desktop
-        {' '}{sessionOrSessions}.  Use the <i>Connect</i> button to establish
-        a connection to a desktop session or the <i>Terminate</i> button to
-        shutdown a desktop session.
+    <div className={`row justify-content-between mb-3`}>
+      <p className={`tagline mb-0`}>
+        You have {sessions.length} desktop {sessionOrSessions} currently running.
       </p>
+      <Link
+        to="/sessions/new"
+        className="button link"
+      >
+        NEW DESKTOP
+      </Link>
     </div>
   );
 }
